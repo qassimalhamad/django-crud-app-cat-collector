716e1aef7251de19d0539ba710e0cdd690fea826
Qasim <qalhamad33@gmail.com>
2024-09-01 09:28:15 +0300

    create

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('cats/', views.cat_index, name='cat-index'),
+  path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+  path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Cat
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 from django.http import HttpResponse
@@ -8,4 +9,18 @@ def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Cat Collector</h1>')
+    return render(request, 'about.html')
+
+
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
