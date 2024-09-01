68dd5f9c2661329234dae50da83f2c0d59eaceea
Qasim <qalhamad33@gmail.com>
2024-09-01 14:44:07 +0300

    many to many

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from .models import Cat
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+from .models import Cat , Toy 
+from django.views.generic import ListView, DetailView # add these 
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
 from django.http import HttpResponse
+
 
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -17,10 +20,74 @@ def cat_index(request):
     cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
 
+from .models import Cat
+# Import the FeedingForm
+from .forms import FeedingForm
+
+
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # toys = Toy.objects.all()
+    # Only get the toys the cat does not have
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have  # send those toys
+    })
 
 class CatCreate(CreateView):
     model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    success_url = '/cats/'
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+    model = Toy
     fields = '__all__'
+
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)
+    cat.toys.remove(toy)  
+    return redirect('cat-detail', cat_id=cat.id)
+
